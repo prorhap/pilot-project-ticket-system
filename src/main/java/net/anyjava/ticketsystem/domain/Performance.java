b347e70f395afcc15cc2893a972203544ca51226
SonHyeonTae <htson@woowahan.com>
2016-04-04 01:15:29 +0900

    BIZPILOT-22 공연등록기능 개발 (엔티티 작성)
    - 엔티티 테스트 작성 및 개발
    - h2 설정
    - properties -> yml 로 변경

--- src/main/java/net/anyjava/ticketsystem/domain/Performance.java
+++ src/main/java/net/anyjava/ticketsystem/domain/Performance.java
@@ -4,7 +4,12 @@ package net.anyjava.ticketsystem.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Setter
@@ -12,9 +17,17 @@ import javax.persistence.Entity;
 public class Performance {
 
 
+    @Id @GeneratedValue
+    @Column(name = "PERFORMANCE_ID")
+    private Long id;
+
     private String title;
 
-    public static Performance createPerformanc(String title) {
+    private LocalDate startDate;
+    private LocalDateTime reservationStartDateTime;
+    private int TotalTicketCount;
+
+    public static Performance createPerformance(String title) {
         Performance performance = new Performance();
         performance.setTitle(title);
 
--- src/test/java/net/anyjava/ticketsystem/domain/PerformanceTest.java
+++ src/test/java/net/anyjava/ticketsystem/domain/PerformanceTest.java
@@ -1,15 +1,37 @@
 package net.anyjava.ticketsystem.domain;
 
+import net.anyjava.TicketApplication;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TicketApplication.class)
+@Transactional
 public class PerformanceTest {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Test
     public void testCreateEntity() {
 
-        Performance performance = Performance.createPerformanc("아이유 콘서트");
+        Performance performance = Performance.createPerformance("아이유 콘서트");
+
+        performance.setStartDate(LocalDate.of(2016, 5, 1));
+        performance.setReservationStartDateTime(LocalDateTime.of(2016, 4, 10, 10, 00, 00));
+        performance.setTotalTicketCount(100);
+
+        em.persist(performance);
 
         assertEquals("아이유 콘서트", performance.getTitle());
     }
